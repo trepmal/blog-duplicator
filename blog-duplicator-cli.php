702a7ab0c676bba3326fd1b5b8f1ff5dfa24b77f
Kailey Lampert <trepmal@gmail.com>
2017-01-01 19:53:22 -0800

    Better args, add verbose

--- blog-duplicator-cli.php
+++ blog-duplicator-cli.php
@@ -10,19 +10,25 @@ class Blog_Duplicate extends WP_CLI_Command {
 	 *
 	 * ## OPTIONS
 	 *
-	 * [<domain-slug>]
+	 * <new-site-slug>
 	 * : The subdomain/directory of the new blog
+	 *
+	 * [--verbose]
+	 * : Output extra info
 	 *
 	 * ## EXAMPLES
 	 *
-	 *     wp duplicate
 	 *     wp duplicate domain-slug
+	 *     wp duplicate test-site-12 --url=multisite.local/test-site-3
 	 */
 	function __invoke( $args, $assoc_args ) {
 
 		if ( ! is_multisite() ) {
 			WP_CLI::error( "This is a multisite command only." );
 		}
+
+		list( $new_slug ) = $args;
+		$verbose = \WP_CLI\Utils\get_flag_value( $assoc_args, 'verbose', false );
 
 		global $wpdb;
 
@@ -33,39 +39,23 @@ class Blog_Duplicate extends WP_CLI_Command {
 
 		global $current_site;
 
- 		if ( ! isset( $args[0] ) ) {
-
-			// prepare question
-			if ( is_subdomain_install() ) {
-	 			$question = '________.' . preg_replace( '|^www\.|', '', $current_site->domain );
-	 		} else {
-	 			$question = $current_site->domain . $current_site->path . '________';
-	 		}
-
-	 		$colorize = WP_CLI::colorize( '%Upress ctrl-D when done%n');
-	 		$blank = WP_CLI::colorize( "%G$question%n");
-			WP_CLI::line( "Provide a site address\nand $colorize\nFill in the blank: $blank" );
-
-			$domain = trim( WP_CLI::get_value_from_arg_or_stdin( false, false ) );
-			$newaddress = str_replace( '________', $domain, $question );
-			WP_CLI::line( "\nNew site address: $newaddress" );
-
-		} else {
-			$domain = $args[0];
-		}
-
 		// new site information
 		if ( is_subdomain_install() ) {
-			$newdomain = $domain . '.' . preg_replace( '|^www\.|', '', $current_site->domain );
+			$newdomain = $new_slug . '.' . preg_replace( '|^www\.|', '', $current_site->domain );
 			$path      = $current_site->path;
 		} else {
 			$newdomain = $current_site->domain;
-			$path      = $current_site->path . $domain . '/';
+			$path      = $current_site->path . $new_slug . '/';
 		}
 
 		// settings to copy from origin site
-		$title = get_bloginfo() .' Copy';
+		$title   = get_bloginfo() .' Copy';
 		$user_id = email_exists( get_option( 'admin_email' ) );
+
+		$this->verbose_line( "New site details:", '', $verbose );
+		$this->verbose_line( '', "    domain -> $new_slug", $verbose );
+		$this->verbose_line( '', "    path   -> $path", $verbose );
+		$this->verbose_line( '', "    title  -> $title", $verbose );
 
 		// first step
 		$id = wpmu_create_blog( $newdomain, $path, $title, $user_id , array( 'public' => 1 ), $current_site->id );
@@ -74,7 +64,7 @@ class Blog_Duplicate extends WP_CLI_Command {
 			WP_CLI::error( $id->get_error_message() );
 		}
 
-		WP_CLI::line( "Duplicating tables..." );
+		$this->verbose_line( "New site id:", $id, $verbose );
 
 		$src_wp_upload_dir = wp_upload_dir();
 		$src_basedir = $src_wp_upload_dir['basedir'];
@@ -89,10 +79,13 @@ class Blog_Duplicate extends WP_CLI_Command {
 			wp_mkdir_p( $dest_basedir );
 
 			// copy files
+			WP_CLI::line( "Duplicating uploads..." );
+			$this->verbose_line( 'Running command:', "rsync -a {$src_basedir}/ {$dest_basedir} --exclude sites", $verbose );
 			shell_exec( "rsync -a {$src_basedir}/ {$dest_basedir} --exclude sites" );
 
 			// duplicate tables
 			$url = home_url();
+			WP_CLI::line( "Duplicating tables..." );
 			foreach ( $wpdb->tables('blog') as $k => $table ) {
 				$origin_table = $origin_tables[ $k ];
 				$wpdb->query( "TRUNCATE TABLE $table" );
@@ -101,14 +94,31 @@ class Blog_Duplicate extends WP_CLI_Command {
 			update_option( 'blogname', $title );
 
 			// long match first, replace upload url
-			WP_CLI::run_command( array( 'search-replace', $src_baseurl, $dest_baseurl ) );
+			WP_CLI::line( "Search-replace'ing tables (1/2)..." );
+			$_command = "search-replace '$src_baseurl' '$dest_baseurl' --url=$url --quiet";
+			$this->verbose_line( 'Running command:', $_command, $verbose );
+			WP_CLI::runcommand( $_command );
+
 			// replace root url
-			WP_CLI::run_command( array( 'search-replace', $origin_url, $url ) );
+			WP_CLI::line( "Search-replace'ing tables (2/2)..." );
+			$_command = "search-replace '$origin_url' '$url' --url=$url --quiet";
+			$this->verbose_line( 'Running command:', $_command, $verbose );
+			WP_CLI::runcommand( $_command );
+
+			WP_CLI::runcommand( "cache flush --url=$url" );
 
 		restore_current_blog();
 
 		WP_CLI::success( "Blog $id created." );
 
+	}
+
+	private function verbose_line( $pre, $text, $verbose=false ) {
+		if ( $verbose ) {
+			WP_CLI::line( WP_CLI::colorize(
+				"%C$pre%n $text"
+			) );
+		}
 	}
 
 }
