1066b112daae63a237082edbe760c2b8335e0119
Derrick Tennant <derrick.tennant@automattic.com>
2020-11-12 12:58:05 -0500

    Adding fixes, filters
    This adds:
    
    * PHPCS Fixes
    * Two new filters
    * Fix/option for sites that do not have local media
    * Fix for Jetpack identity crisis
    * Fix for missing user roles (Thx PR#1)

--- blog-duplicator-cli.php
+++ blog-duplicator-cli.php
@@ -13,6 +13,9 @@ class Blog_Duplicate extends WP_CLI_Command {
 	 * <new-site-slug>
 	 * : The subdomain/directory of the new blog
 	 *
+	 * [--skip-copy-files]
+	 * : Skip copying uploaded files
+	 *
 	 * [--verbose]
 	 * : Output extra info
 	 *
@@ -21,25 +24,31 @@ class Blog_Duplicate extends WP_CLI_Command {
 	 *     wp duplicate domain-slug
 	 *     wp duplicate test-site-12 --url=multisite.local/test-site-3
 	 */
-	function __invoke( $args, $assoc_args ) {
+	public function __invoke( $args, $assoc_args ) {
 
 		if ( ! is_multisite() ) {
-			WP_CLI::error( "This is a multisite command only." );
+			WP_CLI::error( 'This is a multisite command only.' );
 		}
 
 		list( $new_slug ) = $args;
-		$verbose = \WP_CLI\Utils\get_flag_value( $assoc_args, 'verbose', false );
+		$verbose          = \WP_CLI\Utils\get_flag_value( $assoc_args, 'verbose', false );
+		$skip_copy_files  = \WP_CLI\Utils\get_flag_value( $assoc_args, 'skip-copy-files', false );
 
 		global $wpdb;
 
-		// get info for origin site
-		$origin_prefix = $wpdb->prefix;
-		$origin_tables = $wpdb->tables('blog');
-		$origin_url = home_url();
+		// get table info for origin site.
+		$extra_tables = array();
+		foreach ( apply_filters( 'blog_duplicator_extra_tables', array() ) as $extra_table ) {
+			$extra_tables[ $extra_table ] = $wpdb->prefix . $extra_table;
+		}
+
+		$origin_tables  = array_merge( $wpdb->tables( 'blog' ), $extra_tables );
+		$origin_url     = home_url();
+		$original_roles = get_option( $wpdb->prefix . 'user_roles');
 
 		global $current_site;
 
-		// new site information
+		// new site information.
 		if ( is_subdomain_install() ) {
 			$newdomain = $new_slug . '.' . preg_replace( '|^www\.|', '', $current_site->domain );
 			$path      = $current_site->path;
@@ -48,64 +57,111 @@ class Blog_Duplicate extends WP_CLI_Command {
 			$path      = $current_site->path . $new_slug . '/';
 		}
 
-		// settings to copy from origin site
-		$title   = get_bloginfo() .' Copy';
+		// settings to copy from origin site.
+		$title   = get_bloginfo() . ' Copy';
 		$user_id = email_exists( get_option( 'admin_email' ) );
 
-		$this->verbose_line( "New site details:", '', $verbose );
+		$this->verbose_line( 'New site details:', '', $verbose );
 		$this->verbose_line( '', "    domain -> $new_slug", $verbose );
 		$this->verbose_line( '', "    path   -> $path", $verbose );
 		$this->verbose_line( '', "    title  -> $title", $verbose );
 
-		// first step
-		$id = wpmu_create_blog( $newdomain, $path, $title, $user_id , array( 'public' => 1 ), $current_site->id );
+		// first step.
+		$id = wpmu_create_blog( $newdomain, $path, $title, $user_id, array( 'public' => 1 ), $current_site->id );
 
 		if ( is_wp_error( $id ) ) {
 			WP_CLI::error( $id->get_error_message() );
 		}
 
-		$this->verbose_line( "New site id:", $id, $verbose );
+		$this->verbose_line( 'New site id:', $id, $verbose );
 
 		$src_wp_upload_dir = wp_upload_dir();
-		$src_basedir = $src_wp_upload_dir['basedir'];
-		$src_baseurl = $src_wp_upload_dir['baseurl'];
+		$src_basedir       = $src_wp_upload_dir['basedir'];
+		$src_baseurl       = $src_wp_upload_dir['baseurl'];
 
-		// duplicate tables
+		// duplicate tables.
 		switch_to_blog( $id );
-			// make upload destination
-			$dest_wp_upload_dir = wp_upload_dir();
-			$dest_basedir = $dest_wp_upload_dir['basedir'];
-			$dest_baseurl = $dest_wp_upload_dir['baseurl'];
-			wp_mkdir_p( $dest_basedir );
 
-			// copy files
-			WP_CLI::line( "Duplicating uploads..." );
-			$this->verbose_line( 'Running command:', "rsync -a {$src_basedir}/ {$dest_basedir} --exclude sites", $verbose );
-			shell_exec( "rsync -a {$src_basedir}/ {$dest_basedir} --exclude sites" );
+		// make upload destination.
+		$dest_wp_upload_dir = wp_upload_dir();
+		$dest_basedir       = $dest_wp_upload_dir['basedir'];
+		$dest_baseurl       = $dest_wp_upload_dir['baseurl'];
+		wp_mkdir_p( $dest_basedir );
 
-			// duplicate tables
-			$url = home_url();
-			WP_CLI::line( "Duplicating tables..." );
-			foreach ( $wpdb->tables('blog') as $k => $table ) {
-				$origin_table = $origin_tables[ $k ];
-				$wpdb->query( "TRUNCATE TABLE $table" );
-				$wpdb->query( "INSERT INTO $table SELECT * FROM $origin_table" );
+		// copy files.
+		if ( ! $skip_copy_files ) {
+			$is_shell_exec_enabled = is_callable( 'shell_exec' ) && false === stripos( ini_get( 'disable_functions' ), 'shell_exec' );
+
+			if ( ! $is_shell_exec_enabled ) {
+				WP_CLI::warning( 'shell_exec is disabled, skipping file copying!' );
+			} else {
+				$is_rsync_installed = ! empty( shell_exec( 'which rsync' ) ); // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.system_calls_shell_exec
+
+				if ( $is_rsync_installed ) {
+					WP_CLI::line( 'Duplicating uploads...' );
+					$this->verbose_line( 'Running command:', "rsync -a {$src_basedir}/ {$dest_basedir} --exclude sites", $verbose );
+					shell_exec( // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.system_calls_shell_exec
+						sprintf(
+							'rsync -a %s/ %s --exclude sites',
+							escapeshellarg( $src_basedir ),
+							escapeshellarg( $dest_basedir )
+						)
+					);
+				} else {
+					WP_CLI::warning( 'Cannot find rsync, skipping file copying!' );
+				}
 			}
-			update_option( 'blogname', $title );
+		} else {
+			WP_CLI::warning( 'SKIPPING Duplicating uploads...' );
+		}
 
-			// long match first, replace upload url
-			WP_CLI::line( "Search-replace'ing tables (1/2)..." );
-			$_command = "search-replace '$src_baseurl' '$dest_baseurl' --url=$url --quiet";
-			$this->verbose_line( 'Running command:', $_command, $verbose );
-			WP_CLI::runcommand( $_command );
+		// duplicate tables.
+		// phpcs:disable WordPress.DB.DirectDatabaseQuery,WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+		$url = home_url();
+		WP_CLI::line( 'Duplicating tables...' );
 
-			// replace root url
-			WP_CLI::line( "Search-replace'ing tables (2/2)..." );
-			$_command = "search-replace '$origin_url' '$url' --url=$url --quiet";
-			$this->verbose_line( 'Running command:', $_command, $verbose );
-			WP_CLI::runcommand( $_command );
+		$extra_tables = array();
+		foreach ( apply_filters( 'blog_duplicator_extra_tables', array() ) as $extra_table ) {
+			$extra_tables[ $extra_table ] = $wpdb->prefix . $extra_table;
+		}
 
-			WP_CLI::runcommand( "cache flush --url=$url" );
+		$blog_tables = array_merge( $extra_tables, $wpdb->tables( 'blog' ) );
+		foreach ( $blog_tables as $k => $table ) {
+			$origin_table = $origin_tables[ $k ];
+
+			$this->verbose_line( 'Running SQL:', "TRUNCATE TABLE $table", $verbose );
+			$wpdb->query( "TRUNCATE TABLE $table" );
+
+			// Remove blocked options from option table before import.
+			if ( $wpdb->prefix . 'options' === $table ) {
+				$blocked_options = apply_filters( 'blog_duplicator_blocked_options', array( 'jetpack_options', 'jetpack_private_options', 'vaultpress' ) );
+
+				$sql = $wpdb->prepare( "INSERT INTO $table SELECT * FROM $origin_table WHERE option_name NOT IN (" . implode( ', ', array_fill( 0, count( $blocked_options ), '%s' ) ) . ')', ...$blocked_options );
+			} else {
+				$sql = "INSERT INTO $table SELECT * FROM $origin_table";
+			}
+
+			$this->verbose_line( 'Running SQL:', $sql, $verbose );
+			$wpdb->query( $sql ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
+		}
+
+		update_option( 'blogname', $title );
+		update_option( $wpdb->prefix . 'user_roles',  $original_roles );
+		// phpcs:enable WordPress.DB.DirectDatabaseQuery,WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+
+		// long match first, replace upload url.
+		WP_CLI::line( "Search-replace'ing tables (1/2)..." );
+		$_command = "search-replace '$src_baseurl' '$dest_baseurl' --url=$url --quiet --all-tables-with-prefix";
+		$this->verbose_line( 'Running command:', $_command, $verbose );
+		WP_CLI::runcommand( $_command );
+
+		// replace root url.
+		WP_CLI::line( "Search-replace'ing tables (2/2)..." );
+		$_command = "search-replace '$origin_url' '$url' --url=$url --quiet --all-tables-with-prefix";
+		$this->verbose_line( 'Running command:', $_command, $verbose );
+		WP_CLI::runcommand( $_command );
+
+		WP_CLI::runcommand( "cache flush --url=$url" );
 
 		restore_current_blog();
 
@@ -113,11 +169,21 @@ class Blog_Duplicate extends WP_CLI_Command {
 
 	}
 
-	private function verbose_line( $pre, $text, $verbose=false ) {
+	/**
+	 * Outputs extra information.
+	 *
+	 * @param  string  $pre     Text prefix.
+	 * @param  string  $text    Main text to output.
+	 * @param  boolean $verbose Whether or not to output, defaults to false.
+	 * @return void
+	 */
+	private function verbose_line( $pre, $text, $verbose = false ) {
 		if ( $verbose ) {
-			WP_CLI::line( WP_CLI::colorize(
-				"%C$pre%n $text"
-			) );
+			WP_CLI::line(
+				WP_CLI::colorize(
+					"%C$pre%n $text"
+				)
+			);
 		}
 	}
 
